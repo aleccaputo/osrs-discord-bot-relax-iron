ba2b6342347e098e9887622ac42a280202c4bb9d
Alec Caputo <aleccaputo7@gmail.com>
2021-12-01 17:00:26 -0700

    only react with certain numbers on images

--- services/DropSubmissionService.ts
+++ services/DropSubmissionService.ts
@@ -3,38 +3,28 @@ import User from "../models/User";
 import {getUser, modifyNicknamePoints, modifyPoints} from "./UserService";
 
 const NumberEmojis = {
-    ONE: '1️⃣',
+    // ONE: '1️⃣',
     TWO: '2️⃣',
     THREE: '3️⃣',
-    FOUR: '4️⃣',
+    // FOUR: '4️⃣',
     FIVE: '5️⃣',
-    SIX: '6️⃣',
+    // SIX: '6️⃣',
     SEVEN: '7️⃣',
-    EIGHT: '8️⃣',
-    NINE: '9️⃣',
+    // EIGHT: '8️⃣',
+    // NINE: '9️⃣',
     TEN: '🔟'
 }
 // wonder if the intl lib has something for this
 const convertEmojiToNumber = (emoji: Emoji) => {
     switch (emoji.name) {
-        case NumberEmojis.ONE:
-            return 1;
         case NumberEmojis.TWO:
             return 2;
         case NumberEmojis.THREE:
             return 3;
-        case NumberEmojis.FOUR:
-            return 4;
         case NumberEmojis.FIVE:
             return 5;
-        case NumberEmojis.SIX:
-            return 6;
         case NumberEmojis.SEVEN:
             return 7;
-        case NumberEmojis.EIGHT:
-            return 8;
-        case NumberEmojis.NINE:
-            return 9;
         case NumberEmojis.TEN:
             return 10;
         default:
@@ -82,14 +72,14 @@ const processPoints = async (emoji: Emoji, userDiscordId: string, action: Points
 
 export const reactWithBasePoints = async (message: Message) => {
     // stupid that i can't pass an array.
-    await message.react(NumberEmojis.ONE);
+    // await message.react(NumberEmojis.ONE);
     await message.react(NumberEmojis.TWO);
     await message.react(NumberEmojis.THREE);
-    await message.react(NumberEmojis.FOUR);
+    // await message.react(NumberEmojis.FOUR);
     await message.react(NumberEmojis.FIVE);
-    await message.react(NumberEmojis.SIX);
+    // await message.react(NumberEmojis.SIX);
     await message.react(NumberEmojis.SEVEN);
-    await message.react(NumberEmojis.EIGHT);
-    await message.react(NumberEmojis.NINE);
+    // await message.react(NumberEmojis.EIGHT);
+    // await message.react(NumberEmojis.NINE);
     await message.react(NumberEmojis.TEN);
 }
