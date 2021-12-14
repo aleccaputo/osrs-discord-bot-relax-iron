83b0e1b2dd9a098de17cfae504abc6e4f9686600
Alec Caputo <acaputo@mail.smcvt.edu>
2021-12-14 16:12:12 -0700

    verified script

---
+++ migrations/giveVerifiedRole.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+import Discord from "discord.js";
+import {connect} from "../services/DataService";
+
+dotenv.config();
+
+;(async () => {
+    try {
+        const serverId = process.env.SERVER;
+        const client = new Discord.Client();
+
+        await client.login(process.env.TOKEN);
+        await connect();
+
+        const server = await client.guilds.fetch(serverId ?? '')
+        if (server) {
+            const allMembers = await server.members.fetch();
+            for (const member of allMembers.array()) {
+                // if they have at least one role, also give them verified
+                if (member?.roles.cache.array().filter(x => x.name !== '@everyone').length && process.env.VERIFIED_ROLE_ID) {
+                    await member?.roles.add([process.env.VERIFIED_ROLE_ID]);
+                    return;
+                }
+            }
+            process.exit();
+        }
+        console.error("unable to find server");
+        process.exit();
+    } catch (e) {
+        console.log(e);
+    }
+})();
